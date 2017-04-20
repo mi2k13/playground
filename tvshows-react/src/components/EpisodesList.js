980a599994c8e1ceb89c00577ce6315df19486e0
mi2k13 <smoooking.monkey@gmail.com>
2017-04-20 11:58:34 +0200

    [tvshow] Episode air date

--- tvshows-react/src/components/EpisodesList.js
+++ tvshows-react/src/components/EpisodesList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { List } from 'immutable';
 // components
-import EpisodesListItem from './EpisodesListItem';
+import EpisodesListItem from './EpisodesListItem/EpisodesListItem';
 import ListItem from './UI/ListItem/ListItem';
 
 // ============================================
---
+++ tvshows-react/src/components/EpisodesListItem/EpisodesListItem.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Map } from 'immutable';
+// styles
+import styles from './EpisodesListItem.css';
+import classnames from 'classnames/bind';
+const cx = classnames.bind(styles);
+
+// ============================================
+
+const EpisodesListItem = ({ episode }) => {
+  const today = new Date().getTime();
+  const hasBeenAired = new Date(episode.get('airdate')).getTime() < today;
+
+  const episodeNumberFormat = episodeNumber => {
+    if (episodeNumber < 10) {
+      return '0' + episodeNumber;
+    } return episodeNumber;
+  }
+
+  return (
+    <div className={cx(styles.root, { unaired: !hasBeenAired })}>
+      {/* AIR DATE */}
+      <span className={styles.date}>
+        {new Date(episode.get('airdate')).toLocaleDateString()}
+      </span>
+      {/* EPISODE NAME */}
+      <span className={styles.episode}>
+        {`${episodeNumberFormat(episode.get('number'))}. ${episode.get('name')}`}
+      </span>
+    </div>
+  );
+};
+
+EpisodesListItem.propTypes = {
+  episode: React.PropTypes.instanceOf(Map).isRequired,
+};
+
+export default EpisodesListItem;
